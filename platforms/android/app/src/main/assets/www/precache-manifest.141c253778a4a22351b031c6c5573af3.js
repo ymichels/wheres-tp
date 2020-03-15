3542878f3f099cc0e8e5084059307a5a95bda214
Yehuda <yehuda.michelson@gmail.com>
2020-03-15 16:03:22 +0200

    adding cordova

---
+++ platforms/android/app/src/main/assets/www/precache-manifest.141c253778a4a22351b031c6c5573af3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c54ad72cddffa6ff5d231d12b7b486fc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "53486e2f0604a67dee30",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c198f9773d2916e08e45",
+    "url": "./static/js/2.db4cfc5b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.db4cfc5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53486e2f0604a67dee30",
+    "url": "./static/js/main.ebfbcaf6.chunk.js"
+  },
+  {
+    "revision": "fef8a0e0461efd9cdb24",
+    "url": "./static/js/runtime-main.7731908a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
